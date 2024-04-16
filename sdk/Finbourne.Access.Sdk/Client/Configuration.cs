afba8dc259d6102d8249f009d5a73546b7ebae22
Concourse <concourse@finbourne.com>
2024-04-16 15:05:16 +0000

    Updates for v2.0.142

--- sdk/Finbourne.Access.Sdk/Client/Configuration.cs
+++ sdk/Finbourne.Access.Sdk/Client/Configuration.cs
@@ -544,7 +544,7 @@ namespace Finbourne.Access.Sdk.Client
             string report = "C# SDK (Finbourne.Access.Sdk) Debug Report:\n";
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
-            report += "    Version of the API: 0.0.3780\n";
+            report += "    Version of the API: 0.0.3781\n";
             report += "    SDK Package Version: 2.0.0\n";
 
             return report;
