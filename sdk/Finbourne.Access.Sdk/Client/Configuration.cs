b4cfad3c7ca02833d99d18893b28040b9c873bdc
Concourse <concourse@finbourne.com>
2024-10-30 16:08:21 +0000

    access csharp updates for v2.1.157
    Signed-off-by: Concourse <concourse@finbourne.com>

--- sdk/Finbourne.Access.Sdk/Client/Configuration.cs
+++ sdk/Finbourne.Access.Sdk/Client/Configuration.cs
@@ -594,7 +594,7 @@ namespace Finbourne.Access.Sdk.Client
             string report = "C# SDK (Finbourne.Access.Sdk) Debug Report:\n";
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
-            report += "    Version of the API: 0.0.3991\n";
+            report += "    Version of the API: 0.0.3992\n";
             report += "    SDK Package Version: 2.0.0\n";
 
             return report;
